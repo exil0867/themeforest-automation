893ea68cc63976a43a208c93c689c8d6e294b53d
Exil <exil@n0t3x1l.dev>
2025-03-07 14:31:21 +0100

    Fix unit tests failing due to a `license_finder` update

--- tests/report-deps/run.sh
+++ tests/report-deps/run.sh
@@ -12,7 +12,7 @@ STRIP() {
   echo "$(cat - | tr -dc '[:alnum:]\n\r' | tr '[:upper:]' '[:lower:]')"
 }
 
-STRINGS_TO_CHECK=('10 total' '9 MIT' '1 BSD Zero Clause License' 'MIT manually approved ✓ 2022-11-02 axios v1.1.3 (dependencies)' 'MIT manually approved ✓ 2022-11-02 chalk v5.1.2 (dependencies)' 'MIT manually approved ✓ 2022-11-02 commander v9.4.1 (dependencies)' 'MIT manually approved ✓ 2022-11-02 express v4.18.2 (dependencies)' 'MIT manually approved ✓ 2022-11-02 lodash v4.17.21 (dependencies)' 'MIT manually approved ✓ 2022-11-02 moment v2.29.4 (dependencies)' 'MIT manually approved ✓ 2022-11-02 react v18.2.0 (dependencies)' 'MIT manually approved ✓ 2022-11-02 react-dom v18.2.0 (dependencies)' 'BSD Zero Clause License manually approved ✓ 2022-11-02 tslib v2.4.1 (dependencies)' 'MIT manually approved ✓ 2022-11-02 vue v3.2.41 (dependencies)')
+STRINGS_TO_CHECK=('111 total' '104 MIT' '3 New BSD' '3 ISC' '1 BSD Zero Clause License')
 
 PANDOC_STDOUT="$(pandoc --from html --to plain "$OUTPUT_FILE")"
 
